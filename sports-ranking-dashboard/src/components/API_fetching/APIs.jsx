8f40fd934adda842495a32a83874af71fba9ac9c
Sayan Haldar <125077615+HaldarSayan18@users.noreply.github.com>
2024-10-07 10:47:05 +0000

    API,Header2 & Logout file created

---
+++ sports-ranking-dashboard/src/components/API_fetching/APIs.jsx
@@ -0,0 +1,7 @@
+export const regURL = "https://dev8.mydevfactory.com/sport-rank-ftv/public/api/arena/registration";
+
+export const logURL = "https://dev8.mydevfactory.com/sport-rank-ftv/public/api/arena/login";
+
+export const profileUpdateURL = "https://dev8.mydevfactory.com/sport-rank-ftv/public/api/arena/ProfileUpdate";
+
+export const profileViewURL = "https://dev8.mydevfactory.com/sport-rank-ftv/public/api/arena/profile-view";
---
+++ sports-ranking-dashboard/src/components/common/Header2.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { styled } from '@mui/material';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import chat from '../../images/chat.svg';
+import frame from '../../images/Frame.svg';
+import imgDoe from '../../images/image_doe.svg';
+
+const Nav = styled(AppBar)`
+    background:transparent;
+    height:50px;
+    border:none;
+    box-shadow:none;
+    outline:none;
+`
+const Text = styled(Toolbar)`
+    display: grid;
+    justify-items: end;
+    margin-right:50px;
+    overflow:hidden;
+`
+const Typodiv = styled(Typography)`
+    display: flex;
+    justify-content:center;
+    align-items:center;
+    color:#ffd232;
+    ${'' /* height:40px; */}
+    width:100px;
+    margin-top:-5px;
+    font-size:16px;
+    gap: 4px;
+`
+const Header2 = () => {
+    return (
+        <>
+            <Nav position='static'>
+                <Text>
+                    <Typodiv variant="h6" >
+                        <img src={chat} alt='' style={{marginLeft:"-50px"}}/>
+                        <img src={frame} alt='' style={{marginLeft:"10px", marginRight:"10px"}}/>
+                        <img src={imgDoe} alt='' style={{marginRight:"20px"}}/>
+                        <text style={{width:"50px", display:"flex"}}>John Doe</text>
+                        <KeyboardArrowDownIcon />
+                    </Typodiv>
+                </Text>
+            </Nav>
+        </>
+    )
+}
+export default Header2;
