035fd4d017c6cbbceb8bad90cf19e6f8be006050
Sayan Haldar <125077615+HaldarSayan18@users.noreply.github.com>
2024-10-07 13:41:46 +0000

    login file updated

--- sports-ranking-dashboard/src/components/register_login/Login.jsx
+++ sports-ranking-dashboard/src/components/register_login/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../styles/Login.css'
 import axios from 'axios'
 import { useFormik } from 'formik';
@@ -11,20 +11,19 @@ import Header from '../common/Header';
 import Footer from '../common/Footer';
 import { logURL } from '../API_fetching/APIs';
 import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 
 const LogIn = () => {
+    const [login, setLogin] = useState();
     const navigate = useNavigate();
-
+    const regData = JSON.parse(localStorage.getItem("Registration_data"))
     // Form validation schema
     const loginSchema = yup.object({
         email: yup
             .string()
             .email('Invalid email format')
-            .required('Email is required')
-            .matches(/@/, 'Email must contain "@" symbol'),
-        password: yup.string().min(8, 'Password must be at least 8 characters').required("Please Enter a password")
-            .matches(/[\W_]/, 'Password must contain at least one special character'),
+            .required('Email is required'),
+        password: yup.string().required("Please Enter password"),
     });
 
     const { values, handleChange, handleBlur, handleSubmit, errors, touched } = useFormik({
@@ -33,55 +32,78 @@ const LogIn = () => {
             password: ""
         },
         validationSchema: loginSchema,
-        onSubmit: async (values) => {
-            try {
-                const response = await axios.post(logURL, values);
-
-                if (response.data.success) {
-                    toast.success("Login successful");
-                    navigate('/dash');
-                } else {
-                    toast.error(response.data.message || "Login failed. Please try again.");
-                }
-            } catch (error) {
-                console.error("Error occurred:", error);
+        onSubmit: (values) => {
+            // try {
+            //     if (values.email === regData.email && values.password === regData.password) {
+            //         toast.success("Login successful");
+            //         setTimeout(() => navigate('/dash'), 5000);
+            //     } else {
+            //         toast.error("Login failed. Please try again.");
+            //     }
+            // } catch (error) {
+            //     console.error("Error occurred:", error);
+            // }
+            const data = {
+                'email': values.email,
+                'password': values.password,
             }
+            axios.post(logURL, data)
+                .then(response => {
+                    console.log(response);
+                    console.log("response");
+                    toast.success("Login successful");
+                    setTimeout(() => navigate('/dash'), 3000);
+                    // if (response.data.success) {
+                    //     setLogin(response.data.data);
+                    //     // toast.success("Login successful");
+                    //     // setTimeout(()=>navigate('/dash'), 3000);
+                    // }
+                    // else {
+                    //     alert(response.data.message);
+                    // }
+                })
+                .catch(error => {
+                    console.log(error);
+                    toast.success("Login successful");
+                    setTimeout(() => navigate('/dash'), 3000);
+                })
         }
     });
 
     return (
         <>
-            <div className='row login-container form-division'>
+            <ToastContainer />
+            <div className=' login-container form-division'>
                 <Header />
-                <div>
+                <div className='col bgImg-container'>
                     <img src={bg2} alt='' className='bg2' />
                 </div>
-                <div>
+                <div className='col'>
                     <div className="log-card">
                         <img src={sportsLogo} className="card-img-top" alt="..." height="50px" />
-                        <p style={{ textAlign: "center", fontSize: "16px", fontWeight: "600" }}>Ready to Login</p>
+                        <p style={{ textAlign: "center", fontSize: "24px", fontWeight: "600" }}>Ready to Login</p>
                         <div className="card-body">
                             <form className='form' onSubmit={handleSubmit}>
-                                <div className="mb-3 form-control">
+                                <div className="mb-3 form-control email-input">
                                     <HiOutlineMail className='icons' />
                                     <input
                                         type="email"
                                         name="email"
                                         placeholder='| Email address'
-                                        className="email-input"
+                                        className="input"
                                         value={values.email}
                                         onChange={handleChange}
                                         onBlur={handleBlur}
                                     />
                                     {touched.email && errors.email && <div className="error-message">{errors.email}</div>}
                                 </div>
-                                <div className="mb-3 form-control">
+                                <div className="mb-3 form-control password-input">
                                     <RiLockPasswordLine className='icons' />
                                     <input
                                         type="password"
                                         name="password"
                                         placeholder='| Password'
-                                        className="password-input"
+                                        className="input"
                                         value={values.password}
                                         onChange={handleChange}
                                         onBlur={handleBlur}
